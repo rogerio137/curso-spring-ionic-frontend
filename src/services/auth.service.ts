d7436f606b3f9a9f58fe1f823579b7335d54177c
Rogerio Barrucalle <rogerio.137@bol.com.br>
2018-07-24 11:00:54 -0300

    Fazendo o app aproveitar o usuario logado na tela inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -39,4 +39,13 @@ export class AuthService{
         this.storage.setLocalUser(null);
     }
 
+    refreshToken(){
+        return this.http.post(`${API_CONFIG.baseUrl}/auth/refresh_token`, 
+                        {},
+                        {
+                          observe: 'response',
+                          responseType: 'text'  
+                        });
+    }
+
 }
